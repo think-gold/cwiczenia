5e32373ce239c575fe7e8c144545fa42b3975ef1
BeaS84 <131193427+BeaS84@users.noreply.github.com>
2023-04-20 19:45:23 +0200

    Task 4 solved

--- zad1/src/main/java/org/example/App.java
+++ zad1/src/main/java/org/example/App.java
@@ -30,14 +30,41 @@ public class App {
 //        float sum1 = numbers[0] + numbers[1] + numbers[2] + numbers[3];
 //        System.out.println("Suma1: "+sum1);
 
-        float sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum = sum + numbers[i];
-        }
-        System.out.println("suma " + sum);
+//        float sum = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            sum = sum + numbers[i];
+//        }
+//        System.out.println("suma " + sum);
+//
+//        float ave = sum/ numbers.length;
+//        System.out.println(ave);
+//    }
 
-        float ave = sum/ numbers.length;
-        System.out.println(ave);
+    //task 4
+    String[] names = new String[]{"Adam", "Kuba", "Jan", "Kuba", "Jan", "Jan"};
+    Integer counterJan = 0;
+    Integer counterAdam = 0;
+    Integer counterKuba = 0;
+
+
+        for( Integer i = 0 ; i<names.length; i++){
+        if (names[i]=="Jan"){
+            counterJan ++;
+        }else if (names[i]=="Adam"){
+                counterAdam ++;
+            }
+        else {
+            counterKuba++;
+        }
+
     }
+        System.out.println("Kuba: " +counterKuba);
+        System.out.println("Adam: " +counterAdam);
+        System.out.println("Jan: " +counterJan);
+
+
+
+    }
+
 
 }
