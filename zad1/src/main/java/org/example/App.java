531149896f3f7665b87582d3e6c42bb38c4ee491
think-gold <118820315+think-gold@users.noreply.github.com>
2023-04-20 19:35:13 +0200

    task 3 solved

--- zad1/src/main/java/org/example/App.java
+++ zad1/src/main/java/org/example/App.java
@@ -2,32 +2,42 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App
-
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
 
 
         //Exercise 2
-        String[] cities = new String[4];
-        cities[0] = "Paris";
-        cities[1] = "Rome";
-        cities[2] = "Berlin";
-        cities[3] = "Prague";
+//        String[] cities = new String[4];
+//        cities[0] = "Paris";
+//        cities[1] = "Rome";
+//        cities[2] = "Berlin";
+//        cities[3] = "Prague";
+//
+//        for (String city : cities) {
+//            System.out.println(city);
+//        }
+//
+//        for (int i=3; i>=0 ; i--){
+//            System.out.println(cities[i]);
+//        }
+//
+//        for (int i =0 ; i<cities.length; i = i+2){
+//            System.out.println(cities[i]);
+//        }
 
-        for (String city : cities) {
-            System.out.println(city);
-        }
+        Float[] numbers = new Float[]{1.25F, 3.0F, 4.5F, 5.7F};
+//        float sum1 = numbers[0] + numbers[1] + numbers[2] + numbers[3];
+//        System.out.println("Suma1: "+sum1);
 
-        for (int i=3; i>=0 ; i--){
-            System.out.println(cities[i]);
+        float sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
         }
+        System.out.println("suma " + sum);
 
-        for (int i =0 ; i<cities.length; i = i+2){
-            System.out.println(cities[i]);
-        }
+        float ave = sum/ numbers.length;
+        System.out.println(ave);
     }
 
 }
