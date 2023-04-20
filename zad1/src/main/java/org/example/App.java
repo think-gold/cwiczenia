45935d3a3ac8ba0d987d32eba0b95b9572e381f8
think-gold <118820315+think-gold@users.noreply.github.com>
2023-04-20 20:41:18 +0200

    after revision

--- zad1/src/main/java/org/example/App.java
+++ zad1/src/main/java/org/example/App.java
@@ -6,89 +6,86 @@ package org.example;
 public class App {
     public static void main(String[] args) {
 
-
         //Exercise 2
-//        String[] cities = new String[4];
-//        cities[0] = "Paris";
-//        cities[1] = "Rome";
-//        cities[2] = "Berlin";
-//        cities[3] = "Prague";
-//
-//        for (String city : cities) {
-//            System.out.println(city);
-//        }
-//
-//        for (int i=3; i>=0 ; i--){
-//            System.out.println(cities[i]);
-//        }
-//
-//        for (int i =0 ; i<cities.length; i = i+2){
-//            System.out.println(cities[i]);
-//        }
-//Exercise 3
-        Float[] numbers = new Float[]{1.25F, 3.0F, 4.5F, 5.7F};
-//        float sum1 = numbers[0] + numbers[1] + numbers[2] + numbers[3];
-//        System.out.println("Suma1: "+sum1);
 
-//        float sum = 0;
-//        for (int i = 0; i < numbers.length; i++) {
-//            sum = sum + numbers[i];
-//        }
-//        System.out.println("suma " + sum);
-//
-//        float ave = sum/ numbers.length;
-//        System.out.println(ave);
-//    }
+        String[] cities = new String[4];
+        cities[0] = "Paris";
+        cities[1] = "Rome";
+        cities[2] = "Berlin";
+        cities[3] = "Prague";
+
+        for (String city : cities) {
+            System.out.println(city);
+        }
+
+        for (int i = 3; i >= 0; i--) {
+            System.out.println(cities[i]);
+        }
+
+        for (int i = 0; i < cities.length; i = i + 2) {
+            System.out.println(cities[i]);
+        }
+
+        //Exercise 3
+
+        Float[] numbers = new Float[]{1.25F, 3.0F, 4.5F, 5.7F};
+
+        float sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        System.out.println("Sum: " + sum);
+
+        float ave = sum / numbers.length;
+        System.out.println("Average: " + ave);
+
 
         //Exercise 4
-//    String[] names = new String[]{"Adam", "Kuba", "Jan", "Kuba", "Jan", "Jan"};
-//    Integer counterJan = 0;
-//    Integer counterAdam = 0;
-//    Integer counterKuba = 0;
-//
-//
-//        for( Integer i = 0 ; i<names.length; i++){
-//        if (names[i]=="Jan"){
-//            counterJan ++;
-//        }else if (names[i]=="Adam"){
-//                counterAdam ++;
-//            }
-//        else {
-//            counterKuba++;
-//        }
-//
-//    }
-//        System.out.println("Kuba: " +counterKuba);
-//        System.out.println("Adam: " +counterAdam);
-//        System.out.println("Jan: " +counterJan);
+        String[] names = new String[]{"Adam", "Kuba", "Jan", "Kuba", "Jan", "Jan"};
+        Integer counterJan = 0;
+        Integer counterAdam = 0;
+        Integer counterKuba = 0;
+
+
+        for (Integer i = 0; i < names.length; i++) {
+            if (names[i] == "Jan") {
+                counterJan++;
+            } else if (names[i] == "Adam") {
+                counterAdam++;
+            } else {
+                counterKuba++;
+            }
+
+        }
+        System.out.println("Kuba: " + counterKuba);
+        System.out.println("Adam: " + counterAdam);
+        System.out.println("Jan: " + counterJan);
 
         //Exercise 5
 
-//        String[] names = new String[]{"Adam", "Kuba", "Jan", "Zdzichu"};
-//        for (int i = 0; i < names.length; i++) {
-//            if (names[i] == "Jan") {
-//                System.out.println("index Jan: " + i);
-//            }
-//        }
+        String[] namesRandom = new String[]{"Adam", "Kuba", "Jan", "Zdzichu"};
 
-        //exercise 6
+        for (int i = 0; i < namesRandom.length; i++) {
+            if (namesRandom[i] == "Jan") {
+                System.out.println("index Jan: " + i);
+            }
+        }
+
+        //Exercise 6
 
         Integer[] someNumbers = new Integer[]{1, 14, -2, 0, 6};
+
         Integer max = someNumbers[0];
         Integer min = someNumbers[0];
 
-        for (Integer i = 0; i<someNumbers.length; i++){
-            if(someNumbers[i]>max){
+        for (Integer i = 0; i < someNumbers.length; i++) {
+            if (someNumbers[i] > max) {
                 max = someNumbers[i];
-            } else if (someNumbers[i]<min){
+            } else if (someNumbers[i] < min) {
                 min = someNumbers[i];
             }
         }
-        System.out.println("minimum value: "+ min);
-        System.out.println("minimum value: "+ max);
-
-
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
     }
-
-
 }
