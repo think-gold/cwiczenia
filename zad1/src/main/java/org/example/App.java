41650bc1a0af97bf04439c8154738d83569b485d
BeaS84 <131193427+BeaS84@users.noreply.github.com>
2023-04-20 19:16:10 +0200

    Added second task

--- zad1/src/main/java/org/example/App.java
+++ zad1/src/main/java/org/example/App.java
@@ -4,10 +4,22 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
+    
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+
+        //Exercise 2
+        String[] cities = new String[4];
+        cities[0] = "Paris";
+        cities[1] = "Rome";
+        cities[2] = "Berlin";
+        cities[3] = "Prague";
+
+        for (String city : cities) {
+            System.out.println(city);
+        }
     }
+
 }
